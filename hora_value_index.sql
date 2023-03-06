a3606d9113f7074f0311ae7f6194a1fead47ac93
shintaro-minowa <shintaro-minowa@users.noreply.github.com>
2023-03-07 00:05:16 +0900

    和了価値指標のSQLファイルを追加

---
+++ hora_value_index.sql
@@ -0,0 +1,192 @@
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.13', '1', '1', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.19', '1', '2', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.25', '1', '3', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.32', '1', '4', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.36', '1', '5', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.46', '1', '6', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.15', '1', '1', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.13', '1', '2', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.13', '1', '3', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.15', '1', '4', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.17', '1', '5', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.19', '1', '6', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.98', '1', '1', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.93', '1', '2', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.86', '1', '3', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.85', '1', '4', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.79', '1', '5', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.72', '1', '6', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.74', '1', '1', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.75', '1', '2', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.72', '1', '3', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.68', '1', '4', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.64', '1', '5', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.61', '1', '6', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.96', '2', '1', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.11', '2', '2', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.19', '2', '3', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.31', '2', '4', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.4', '2', '5', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.57', '2', '6', '15000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('1.02', '2', '1', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.98', '2', '2', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.91', '2', '3', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.86', '2', '4', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.8', '2', '5', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.71', '2', '6', '25000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.68', '2', '1', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.64', '2', '2', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.57', '2', '3', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.56', '2', '4', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.51', '2', '5', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.45', '2', '6', '35000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.48', '2', '1', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.47', '2', '2', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.44', '2', '3', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.4', '2', '4', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.35', '2', '5', '45000');
+INSERT INTO "app_hora_value_index"
+    ("value", "rule", "ba", "own_point")
+VALUES
+    ('0.3', '2', '6', '45000');
